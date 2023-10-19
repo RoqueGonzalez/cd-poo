465be33feab1f754ac7c0d75e20d5767111c4a54
RoqueGonzalez <roqueglez9@gmail.com>
2023-10-19 10:32:44 +0200

    Add exercise 3

---
+++ src/main/java/com/campusdual/exercisespoo/Exercise03.java
@@ -0,0 +1,36 @@
+package com.campusdual.exercisespoo;
+
+public class Exercise03 {
+
+    public static String saludo(String nombre){
+
+    String mensaje = "Hola. bienvenido, "+ nombre;
+    return mensaje;
+    }
+
+public static void fechaActual(String fecha){
+    System.out.println(fecha);
+
+}
+
+
+    public static void main(String[] args) {
+        System.out.println(saludo("Roque"));
+        fechaActual("19/10/2023");
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+}
