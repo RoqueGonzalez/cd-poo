c874873f583168d9759b5357b8ca1d23c09211c0
RoqueGonzalez <roqueglez9@gmail.com>
2023-10-23 11:08:43 +0200

    Update Exercise05

--- src/main/java/com/campusdual/exercisespoo/Exercise05.java
+++ src/main/java/com/campusdual/exercisespoo/Exercise05.java
@@ -11,7 +11,7 @@ public class Exercise05 {
         System.out.print("Comprobar si un nº es multiplo de otro: ");
         esMultiplo(4,2);
         System.out.print("Comprobar si es mayor o menor: ");
-        esMayor(5,2);
+        esMayor(4,4);
     }
 public static void esPositivo(int numero){
         if (numero>=0){
@@ -28,16 +28,20 @@ public static void esPositivo(int numero){
             System.out.println(num1 + " No es multiplo de " + num2);
         }
    }
-public static void esMayor (int n,int n2){
-       if (n > n2){
-           System.out.println("Es mayor");
-       }else{
-           System.out.println("Es menor");
-       }
-}
+public static void esMayor (int numero1,int numero2){
+    if (numero1 < numero2) {
+        System.out.println(numero1 + " es menor que " + numero2);
+    } else if (numero1 > numero2) {
+        System.out.println(numero1 + " es mayor que " + numero2);
+    } else {
+        System.out.println(numero1 + " es igual a " + numero2);
+    }
+    }
+    }
 
 
-}
+
+
 
 
 
